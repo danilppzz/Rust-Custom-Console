29ed9b3793d33926b6ac09c6e207337e18faa972
d4niz <josedanielmorebolopez@gmail.com>
2023-11-01 23:52:42 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-fn main() {
+fn main() {    
     Logger::info("This is an informational message");
     Logger::warn("This is a warning message");
     Logger::error("This is an error message");
     Logger::success("This is a success message");
+    Logger::new(4);
 }
 
 struct Logger;
 
 impl Logger {
+    fn new(times: u8) {
+        for _ in 0..times {
+            println!("\x1b[37m\x1b[0m");
+        }
+    }
     fn info(message: &str) {
         println!("\x1b[37m [INFO] {}\x1b[0m", message);
     }
