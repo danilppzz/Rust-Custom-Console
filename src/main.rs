9f620d910d3f5effaff9e9bfe010d73d848c2189
d4niz <josedanielmorebolopez@gmail.com>
2023-11-03 12:10:27 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,47 @@
-use std::process::Command;
+use std::{io::{self, Write}};
+
+struct ConsoleThread;
+
+impl ConsoleThread {
+    fn stop_console() {
+        STOP.store(true, std::sync::atomic::Ordering::Relaxed);
+    }
+
+    fn run() {
+        println!("soizx@1.0");
+        while !STOP.load(std::sync::atomic::Ordering::Relaxed) {
+            let mut input = String::new();
+            print!("\x1b[37m>\x1b[0m ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut input).unwrap();
+            let command = input.trim();
+
+            if command == "stop" {
+                Logger::warn("Press Enter to close...");
+                ConsoleThread::stop_console();
+                break;
+            } else if command == "done" {
+                Logger::success("Done")
+            } else {
+                Logger::error("Invalid Command")
+            }
+        }
+
+        // Wait for Enter to close the console
+        let mut enter_input = String::new();
+        io::stdin().read_line(&mut enter_input).unwrap();
+    }
+}
+
+static STOP: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
 
 fn main() {
-    Logger::info("This is an informational message");
-    Logger::warn("This is a warning message");
-    Logger::error("This is an error message");
-    Logger::success("This is a success message");
-    Logger::new(4);
-
-    let _ = Command::new("cmd")
-        .args(&["/C", "pause"])
-        .status();
+    ConsoleThread::run();
 }
 
 struct Logger;
 
 impl Logger {
-    fn new(times: u8) {
-        for _ in 0..times {
-            println!("\x1b[37m\x1b[0m");
-        }
-    }
     fn info(message: &str) {
         println!("\x1b[37m [INFO] {}\x1b[0m", message);
     }
